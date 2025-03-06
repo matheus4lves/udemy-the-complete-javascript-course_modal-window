046cf4384e6fdc62a99ede0fc806965325e57138
Matheus Alves <matheusalves.bmd@gmail.com>
2025-03-06 02:58:42 -0300

    Fix configuration errors
    Changes summary:
    
     - http-server was replaced by live-server, which provides live reload
       and hot module replacement;
    
     - nodemon was uninstalled and gulp's watch mode was used instead;
    
     - The defer attribute of the script tag was added to the correct place;
    
     - The build directory was git ignored.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-import { src, dest, series } from "gulp";
+import { src, dest, series, watch, lastRun } from "gulp";
 import ts from "gulp-typescript";
 
 const tsESM = ts.createProject("./tsconfig.json");
 
 function compileTS() {
-  return src("./app/**/*.ts").pipe(tsESM()).pipe(dest("dist/"));
+  return src("./app/**/*.ts", { since: lastRun(compileTS) })
+    .pipe(tsESM())
+    .pipe(dest("dist/"));
 }
 
 function copy() {
-  return src(["./app/**/*.html", "app/**/*.css"]).pipe(dest("dist/"));
+  return src("./app/**/*.{html,css}", { since: lastRun(copy) }).pipe(
+    dest("dist/")
+  );
+}
+
+function watchFiles() {
+  watch("./app/**/*.ts", compileTS);
+  watch("./app/**/*.{html,css}", copy);
 }
 
 const build = series(compileTS, copy);
 
-export default build;
+export { build, watchFiles };
