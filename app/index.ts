f3976eac4b955d5583154836af282cf4e48851bd
Matheus Alves <matheusalves.bmd@gmail.com>
2025-03-13 23:58:06 -0300

    Handle modal opening

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,14 @@
-'use strict';
+// DOM manipulation
+const openModalBtns = document.querySelectorAll(".show-modal");
+
+const modal = document.querySelector(".modal") as HTMLDivElement;
+
+// Functionality definition
+function showModal() {
+  modal.classList.remove("hidden");
+}
+
+// Event handling
+openModalBtns.forEach(openModelBtn =>
+  openModelBtn.addEventListener("click", showModal)
+);
