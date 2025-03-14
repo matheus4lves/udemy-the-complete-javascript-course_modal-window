ff64fc9a44140ed416732c30a1bd1ea7496675cf
Matheus Alves <matheusalves.bmd@gmail.com>
2025-03-13 23:59:12 -0300

    Handle close button click

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,8 @@
 // DOM manipulation
 const openModalBtns = document.querySelectorAll(".show-modal");
-
+const closeModalBtn = document.querySelector(
+  ".close-modal"
+) as HTMLButtonElement;
 const modal = document.querySelector(".modal") as HTMLDivElement;
 
 // Functionality definition
@@ -8,7 +10,21 @@ function showModal() {
   modal.classList.remove("hidden");
 }
 
+function hideModal(modal: HTMLDivElement) {
+  modal.classList.add("hidden");
+}
+
+function handleCloseButtonClick(event: MouseEvent) {
+  const target = event.target as HTMLButtonElement;
+
+  if (target.classList.contains("close-modal")) {
+    hideModal(modal);
+  }
+}
+
 // Event handling
 openModalBtns.forEach(openModelBtn =>
   openModelBtn.addEventListener("click", showModal)
 );
+
+closeModalBtn.addEventListener("click", handleCloseButtonClick);
